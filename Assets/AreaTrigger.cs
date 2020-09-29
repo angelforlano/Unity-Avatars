28bcc68296561892cd3a0695ebb258cbf08953b3
Angel-Lap <angel12345leo@gmail.com>
2020-09-29 22:47:11 +0200

    first commit

---
+++ Assets/AreaTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaTrigger : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerController>().speed = 2;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerController>().speed = 4;
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Controllers")]
+    public bool canMove = true;
+
+    [Header("Player Settings")]
+    [Range(0, 100)] public int hp = 100;
+    [Range(1, 5)] public float speed = 4;
+    
+    [Header("Player Stacks")]
+    public int coins;
+    public int keys;
+
+    void Update()
+    {
+        // solo nos moveremos si (canMove igual a true) y (hp mayor a 0).
+        if (canMove && hp > 0)
+        {
+           Move(); 
+        }
+    }
+
+    void Move()
+    {
+        var hMove = Input.GetAxis("Horizontal");
+        var vMove = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(hMove, 0, vMove) * Time.deltaTime * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coins"))
+        {
+            coins++;
+            Destroy(other.gameObject);
+        }
+
+        /*if (other.CompareTag("Keys"))
+        {
+            keys++;
+            Destroy(other.gameObject);
+        }*/
+    }
+}
